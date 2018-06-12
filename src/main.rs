e43b0b30862cb689a93132f0a61e19cd7fe53f01
Philipp Hansch <dev@phansch.net>
2018-06-12 20:09:50 +0200

    Add basic clap cli parsing setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
+    let matches = App::new("jours")
+        .version("0.0.1")
+        .author("Philipp Hansch <dev@phansch.net>")
+        .about("Daily journals and note taking on the command line")
+        .arg(Arg::with_name("add")
+             .short("a")
+             .long("add")
+             .value_name("TEXT")
+             .help("Add an entry")
+             .takes_value(true))
+        .get_matches();
+
+
     println!("Hello, world!");
 }
